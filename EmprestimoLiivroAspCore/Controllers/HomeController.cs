68d02fc313052151aea72c5b25919092ea93bf37
Nilson Alcides <nilson_alcides@hotmail.com>
2023-12-17 18:23:54 -0300

    Add itens carrinho de emprestimo

--- EmprestimoLiivroAspCore/Controllers/HomeController.cs
+++ EmprestimoLiivroAspCore/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using EmprestimoLiivroAspCore.Models;
+﻿using EmprestimoLiivroAspCore.CarrinhoCompra;
+using EmprestimoLiivroAspCore.Models;
 using EmprestimoLiivroAspCore.Repository.Contrato;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
@@ -9,17 +10,42 @@ namespace EmprestimoLiivroAspCore.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private ILivroRepository _livroRepository;
-        public HomeController(ILogger<HomeController> logger, ILivroRepository livroRepository)
+        private CookieCarrinhoCompra _cookieCarrinhoCompra;
+        public HomeController(ILogger<HomeController> logger, ILivroRepository livroRepository, CookieCarrinhoCompra cookieCarrinhoCompra)
         {
             _logger = logger;
             _livroRepository = livroRepository;
+            _cookieCarrinhoCompra = cookieCarrinhoCompra;
         }
 
         public IActionResult Index()
         {
             return View(_livroRepository.ObterTodosLivros()); 
         }
+        //Item ID = ID Produto
+        public IActionResult AdicionarItem(int id)
+        {
+            Livro produto = _livroRepository.ObterLivros(id);
 
+            if (produto == null)
+            {
+                return View("NaoExisteItem");
+            }
+            else
+            {
+                var item = new Livro()
+                {
+                    codLivro = id,
+                    quantidade = 1,
+                    imagemLivro = produto.imagemLivro,
+                    nomeLivro = produto.nomeLivro
+                };
+                _cookieCarrinhoCompra.Cadastrar(item);
+
+                return RedirectToAction(nameof(Index));
+            }
+
+        }
 
         public IActionResult Privacy()
         {
--- EmprestimoLiivroAspCore/Controllers/LivrosController.cs
+++ EmprestimoLiivroAspCore/Controllers/LivrosController.cs
@@ -32,5 +32,6 @@ namespace EmprestimoLiivroAspCore.Controllers
 
             return View();
         }
+
     }
 }
