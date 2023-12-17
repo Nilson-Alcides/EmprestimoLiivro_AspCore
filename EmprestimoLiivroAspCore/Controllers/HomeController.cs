7aabec51b715fbeac5331fd8af717def2fad59c5
Nilson Alcides <nilson_alcides@hotmail.com>
2023-12-17 18:46:31 -0300

    Add empréstimo + itens do empréstimo

--- EmprestimoLiivroAspCore/Controllers/HomeController.cs
+++ EmprestimoLiivroAspCore/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using EmprestimoLiivroAspCore.CarrinhoCompra;
 using EmprestimoLiivroAspCore.Models;
+using EmprestimoLiivroAspCore.Repository;
 using EmprestimoLiivroAspCore.Repository.Contrato;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
@@ -11,11 +12,16 @@ namespace EmprestimoLiivroAspCore.Controllers
         private readonly ILogger<HomeController> _logger;
         private ILivroRepository _livroRepository;
         private CookieCarrinhoCompra _cookieCarrinhoCompra;
-        public HomeController(ILogger<HomeController> logger, ILivroRepository livroRepository, CookieCarrinhoCompra cookieCarrinhoCompra)
+
+        private IEmprestimoRepository _emprestimoRepository;
+        private IItemRepository _itemRepository;
+        public HomeController(ILogger<HomeController> logger, ILivroRepository livroRepository, CookieCarrinhoCompra cookieCarrinhoCompra, IEmprestimoRepository emprestimoRepository, IItemRepository itemRepository)
         {
             _logger = logger;
             _livroRepository = livroRepository;
             _cookieCarrinhoCompra = cookieCarrinhoCompra;
+            _emprestimoRepository = emprestimoRepository;
+            _itemRepository = itemRepository;
         }
 
         public IActionResult Index()
@@ -60,6 +66,37 @@ namespace EmprestimoLiivroAspCore.Controllers
         {
             _cookieCarrinhoCompra.Remover(new Livro() { codLivro = id });
             return RedirectToAction(nameof(Carrinho));
+        }
+
+        DateTime data;
+        public IActionResult SalvarCarrinho(Emprestimo emprestimo)
+        {
+            List<Livro> carrinho = _cookieCarrinhoCompra.Consultar();
+
+
+            Emprestimo mdE = new Emprestimo();
+            Item mdI = new Item();
+
+            data = DateTime.Now.ToLocalTime();
+
+            mdE.dtEmpre = data.ToString("dd/MM/yyyy");
+            mdE.dtDev = data.AddDays(7).ToString();
+            mdE.codUsu = "1";
+            _emprestimoRepository.Cadastrar(mdE);
+
+            _emprestimoRepository.buscaIdEmp(emprestimo);
+
+            for (int i = 0; i < carrinho.Count; i++)
+            {
+
+                mdI.codEmp = Convert.ToInt32(emprestimo.codEmp);
+                mdI.codLivro = Convert.ToString(carrinho[i].codLivro);
+
+                _itemRepository.Cadastrar(mdI);
+            }
+
+            _cookieCarrinhoCompra.RemoverTodos();
+            return RedirectToAction("confEmp");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
