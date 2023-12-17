51c979c25a9c29b2cdb693081d782bffddd173c4
Nilson Alcides <nilson_alcides@hotmail.com>
2023-12-17 15:05:37 -0300

    Cadastrar cookie

---
+++ EmprestimoLiivroAspCore/Cookie/Cookie.cs
@@ -0,0 +1,27 @@
+﻿namespace EmprestimoLiivroAspCore.Cookie
+{
+    public class Cookie
+    {
+        private IHttpContextAccessor _context;
+        private IConfiguration _configuration;
+
+        public Cookie(IHttpContextAccessor httpContextAccessor, IConfiguration configuration)
+        {
+            _context = httpContextAccessor;
+            _configuration = configuration;
+        }
+        /*
+         * CRUD - Cadastrar/Atualizar/Consultar/Remover - RemoverTodos/Exist
+         */
+
+        // Cadastrar Cookie
+        public void Cadastrar(string Key, string Valor)
+        {
+            CookieOptions Options = new CookieOptions();
+            Options.Expires = DateTime.Now.AddDays(7);
+            Options.IsEssential = true;
+
+            _context.HttpContext.Response.Cookies.Append(Key, Valor, Options);
+        }
+    }
+}
--- EmprestimoLiivroAspCore/Program.cs
+++ EmprestimoLiivroAspCore/Program.cs
@@ -7,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddHttpContextAccessor();
 
 //Injeção de dependencia 
 builder.Services.AddScoped<ILivroRepository, LivroRepository>();
