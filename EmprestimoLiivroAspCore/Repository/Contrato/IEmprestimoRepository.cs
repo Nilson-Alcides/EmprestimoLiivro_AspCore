720c6a42096c0af2ba9c8aa823a09bfc4e84c168
Nilson Alcides <nilson_alcides@hotmail.com>
2023-12-17 14:33:35 -0300

    interface  do empréstimo

---
+++ EmprestimoLiivroAspCore/Repository/Contrato/IEmprestimoRepository.cs
@@ -0,0 +1,20 @@
+﻿using EmprestimoLiivroAspCore.Models;
+
+namespace EmprestimoLiivroAspCore.Repository.Contrato
+{
+    public interface IEmprestimoRepository
+    { 
+        //CRUD
+        IEnumerable<Emprestimo> ObterTodosEmprestimos();
+        
+        void Cadastrar(Emprestimo emprestimo);
+       
+        void Atualizar(Emprestimo emprestimo);
+        
+        Emprestimo ObterEmprestimos(int Id);
+        
+        void buscaIdEmp(Emprestimo emprestimo);
+        
+        void Excluir(int Id);
+    }
+}
