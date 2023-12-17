4384295a279540b1ca2a18bda9693af6cfea1ac8
Nilson Alcides <nilson_alcides@hotmail.com>
2023-12-17 14:48:56 -0300

    interface Item

---
+++ EmprestimoLiivroAspCore/Repository/Contrato/IItemRepository.cs
@@ -0,0 +1,18 @@
+﻿using EmprestimoLiivroAspCore.Models;
+
+namespace EmprestimoLiivroAspCore.Repository.Contrato
+{
+    public interface IItemRepository
+    {
+        //CRUD
+        IEnumerable<Item> ObterTodosItens();
+
+        void Cadastrar(Item item);
+
+        void Atualizar(Item item);
+
+        Item ObterItens(int Id);
+
+        void Excluir(int Id);
+    }
+}
