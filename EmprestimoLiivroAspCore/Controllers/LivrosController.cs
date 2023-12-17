f4b115de8349d99fbaf6af4061a694315465d33e
Nilson Alcides <nilson_alcides@hotmail.com>
2023-12-17 13:53:49 -0300

    Livro controller + Cadastro Livro

---
+++ EmprestimoLiivroAspCore/Controllers/LivrosController.cs
@@ -0,0 +1,34 @@
+﻿using EmprestimoLiivroAspCore.GerenciaArquivos;
+using EmprestimoLiivroAspCore.Models;
+using EmprestimoLiivroAspCore.Repository.Contrato;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmprestimoLiivroAspCore.Controllers
+{
+    public class LivrosController : Controller
+    {
+        private readonly ILogger<LivrosController> _logger;
+        private ILivroRepository _livroRepository;
+        public LivrosController(ILogger<LivrosController> logger, ILivroRepository livroRepository)
+        {
+            _logger = logger;
+            _livroRepository = livroRepository;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(Livro livro, IFormFile file)
+        {
+            var Caminho = GerenciadorArquivo.CadastrarImagemProduto(file);
+
+            livro.imagemLivro = Caminho;
+            _livroRepository.Cadastrar(livro);
+
+            ViewBag.msg = "Cadastro realizado com sucesso";
+            return View();
+
+        }
+    }
+}
