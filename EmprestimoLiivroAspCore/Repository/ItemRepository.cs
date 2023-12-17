d8b076518aded68b7506ea58fe89d381bcf2709d
Nilson Alcides <nilson_alcides@hotmail.com>
2023-12-17 14:50:42 -0300

    Implementar classe Item Repository

---
+++ EmprestimoLiivroAspCore/Repository/ItemRepository.cs
@@ -0,0 +1,33 @@
+﻿using EmprestimoLiivroAspCore.Models;
+using EmprestimoLiivroAspCore.Repository.Contrato;
+
+namespace EmprestimoLiivroAspCore.Repository
+{
+    public class ItemRepository : IItemRepository
+    {
+        public void Atualizar(Item item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Cadastrar(Item item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Excluir(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Item ObterItens(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Item> ObterTodosItens()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
